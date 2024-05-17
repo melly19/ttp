74b822498b54267b948c19d7e77e82939e813da4
melly19 <melaniepoon.2022@scis.smu.edu.sg>
2024-05-17 22:08:33 +0800

    #2: Added login screen and authentication module

---
+++ frontend/android/app/src/main/java/com/ttp/AuthenticationModule.kt
@@ -0,0 +1,68 @@
+package com.ttp
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import com.google.firebase.Firebase
+
+class AuthModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    override fun getName(): String {
+        return "AuthModule"
+    }
+
+    @ReactMethod
+    fun createUserWithEmail(email: String, password: String, promise: Promise) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    promise.resolve(auth.currentUser?.uid)
+                } else {
+                    promise.reject("CREATE_USER_ERROR", task.exception?.message ?: "Unknown error")
+                }
+            }
+    }
+
+    @ReactMethod
+    fun signInWithEmail(email: String, password: String, promise: Promise) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    promise.resolve(auth.currentUser?.uid)
+                } else {
+                    promise.reject("SIGN_IN_ERROR", task.exception?.message ?: "Unknown error")
+                }
+            }
+    }
+
+    @ReactMethod
+    fun sendPasswordResetEmail(email: String, promise: Promise) {
+        auth.sendPasswordResetEmail(email)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    promise.resolve("Email sent")
+                } else {
+                    promise.reject("RESET_EMAIL_ERROR", task.exception?.message ?: "Unknown error")
+                }
+            }
+    }
+
+    @ReactMethod
+    fun sendEmailVerification(promise: Promise) {
+        val user = auth.currentUser
+        user?.sendEmailVerification()?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                promise.resolve("Verification email sent")
+            } else {
+                promise.reject("EMAIL_VERIFICATION_ERROR", task.exception?.message ?: "Unknown error")
+            }
+        }
+    }
+}
+
