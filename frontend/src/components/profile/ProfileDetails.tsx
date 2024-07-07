67b1d2225a495a79414a820f3b1dc30546f0cc06
melly19 <melaniepoon.2022@scis.smu.edu.sg>
2024-07-07 21:55:21 +0800

    #3: Added profile screen components

---
+++ frontend/src/components/profile/ProfileDetails.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const ProfileDetails = ({ profile }) => {
+    return (
+        <View style={styles.container}>
+            <Image source={{ uri: "https://i2.cdn.turner.com/money/dam/assets/170210115106-pollinating-bee-340xa.jpg" }} style={styles.avatar} />
+            <Text style={styles.title}>Your Profile Information</Text>
+            <Text style={styles.detail}>Name: {profile.name}</Text>
+            <Text style={styles.detail}>Gender: {profile.gender}</Text>
+            <Text style={styles.detail}>Age group: {profile.ageGroup}</Text>
+            <Text style={styles.detail}>Position: {profile.position}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    avatar: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        marginBottom: 40,
+        borderWidth: 4,
+        borderColor: '#aaa'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 15
+    },
+    detail: {
+        marginBottom: 10,
+        fontSize: 18
+    }
+});
+
+export default ProfileDetails;
---
+++ frontend/src/components/profile/ProfileFormModal.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { Modal, View, TextInput, Text, Button, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+
+const ProfileFormModal = ({ modalVisible, setModalVisible, profile, setProfile, handleSave }) => {
+    return (
+        <Modal
+            visible={modalVisible}
+            animationType="fade"
+            onRequestClose={() => setModalVisible(false)}
+        >
+            <View style={styles.modalContent}>
+                <Text style={styles.title}>Change your profile details here!</Text>
+                <Text style={styles.label}>Name:</Text>
+                <TextInput style={styles.input} value={profile.name} onChangeText={(text) => setProfile({ ...profile, name: text })} placeholder="Name" />
+                <Text style={styles.label}>Gender:</Text>
+                <View style={styles.pickerContainer}>
+                    <Picker
+                        selectedValue={profile.gender}
+                        onValueChange={(itemValue) => setProfile({...profile, gender: itemValue})}
+                        style={styles.picker}
+                    >
+                        <Picker.Item label="Male" value="Male" />
+                        <Picker.Item label="Female" value="Female" />
+                        <Picker.Item label="Other" value="Other" />
+                    </Picker>
+                </View>
+                <Text style={styles.label}>Age group:</Text>
+                <View style={styles.pickerContainer}>
+                    <Picker
+                        selectedValue={profile.ageGroup}
+                        onValueChange={(itemValue) => setProfile({...profile, ageGroup: itemValue})}
+                        style={styles.picker}
+                    >
+                        <Picker.Item label="<18" value="<18" />
+                        <Picker.Item label="18-25" value="18-25" />
+                        <Picker.Item label="26-35" value="26-35" />
+                        <Picker.Item label="36-45" value="36-45" />
+                        <Picker.Item label="46-55" value="46-55" />
+                        <Picker.Item label="56-65" value="56-65" />
+                        <Picker.Item label=">65" value=">65" />
+                    </Picker>
+                </View>
+                <Text style={styles.label}>Position:</Text>
+                <TextInput style={styles.input} value={profile.position} onChangeText={(text) => setProfile({ ...profile, position: text })} placeholder="Position" />
+                <View style={styles.buttonContainer}>
+                    <Button title="Save Changes" onPress={handleSave} />
+                </View>
+                <View style={styles.buttonContainer}>
+                    <Button title="Cancel" onPress={() => setModalVisible(false)} color="#FF6347" />
+                </View>
+            </View>
+        </Modal>
+    );
+};
+
+const styles = StyleSheet.create({
+    modalContent: {
+        height: '60%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 150,
+        padding: 20,
+        backgroundColor: 'white',
+        width: '80%',
+        alignSelf: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        borderRadius: 10
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 20
+    },
+    input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginBottom: 10,
+        padding: 10,
+        width: '100%',
+        fontSize: 16
+    },
+    label: {
+        fontSize: 16,
+        marginBottom: 5,
+        alignSelf: 'flex-start'
+    },
+    picker: {
+        width: '100%',
+        height: 40
+    },
+    pickerContainer: {
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: 'gray',
+        width: '100%',
+        justifyContent: 'center',
+        marginBottom: 10
+    },
+    buttonContainer: {
+        width: '100%',
+        marginTop: 15
+    }
+});
+
+export default ProfileFormModal;
